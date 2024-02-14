1d7065deafb10013fa07c5890b9c877d38886858
mdsajalali <mdsajalali2020@gmail.com>
2024-02-14 15:04:35 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5pbhxs.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const productCollection = client
+      .db("media-centerDB")
+      .collection("products");
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Media Center Server is running...");
@@ -15,5 +46,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
 });
-
-
